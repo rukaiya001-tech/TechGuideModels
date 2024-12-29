8cd8dbd2eaa324e18429829551bd1977457edda5
Rukaiya Islam <rukaiyaislam353@gmail.com>
2024-12-29 13:24:46 +0600

    Update Program.cs
    Change

--- TechGuide/Program.cs
+++ TechGuide/Program.cs
@@ -1,10 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using TechGuide.Models;
+using TechGuide.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TechGuideDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string '' not found.")));
-
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddControllers().AddJsonOptions(options =>
@@ -13,10 +13,7 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
